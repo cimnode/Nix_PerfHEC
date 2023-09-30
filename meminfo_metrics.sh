a314793dc9f9e55ad46207bc0b14fbf850e2dead
cimnode <49315989+cimnode@users.noreply.github.com>
2023-09-30 14:57:35 -0400

    Update meminfo_metrics.sh

--- meminfo_metrics.sh
+++ meminfo_metrics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 splunk_server_and_port="your server address:port_number"
 hec_token="your hec token"
-splunk_index="your index"
+metrics_index="your index"
 
 for line in $(cat /proc/meminfo | grep "MemTotal\|MemFree\|SwapTotal\|SwapFree" | sed -r 's/^(\w+):\s+([0-9]+).*$/\1\=\2/'); 
 do 
@@ -10,4 +10,4 @@ done
 
 curl -k "https://'${splunk_server_and_port}'/services/collector" \
     -H "Authorization: '${hec_token}'" \
-    -d '{"event":"metric","fields": {"metric_name:MemTotalkB":"'${MemTotal}'","metric_name:MemFreekB":"'${MemFree}'","metric_name:SwapFreekB":"'${SwapFree}'","metric_name:SwapTotalkB":"'${SwapTotal}'"},"index":"'${splunk_index}'","host":"'${HOSTNAME}'", "sourcetype": "meminfo", "source":"/proc/meminfo","time":"'$(date +%s)'"}'
+    -d '{"event":"metric","fields": {"metric_name:MemTotalkB":"'${MemTotal}'","metric_name:MemFreekB":"'${MemFree}'","metric_name:SwapFreekB":"'${SwapFree}'","metric_name:SwapTotalkB":"'${SwapTotal}'"},"index":"'${metrics_index}'","host":"'${HOSTNAME}'", "sourcetype": "meminfo", "source":"/proc/meminfo","time":"'$(date +%s)'"}'
