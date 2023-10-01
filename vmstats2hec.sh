e28fa914e90418ee8f333847a857381314fd0e35
cimnode <49315989+cimnode@users.noreply.github.com>
2023-10-01 09:57:52 -0400

    reorg to ansible role based layout

--- vmstats2hec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-splunk_server="your server address"
-hec_token="your hec token"
-splunk_index="your index"
-
-/usr/bin/python /root/scripts/vmstat_json.py | /usr/bin/python /root/scripts/json2hec.py ${hec_token} ${splunk_server} '{"index":"'${splunk_index}'", "host":"'${HOSTNAME}'", "source":"vmstat", "sourcetype":"vmstat-json"}'
